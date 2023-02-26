f37a832040a0684a5616f1800c76fec500f06980
dev01team <125609175+dev01team@users.noreply.github.com>
2023-02-26 06:30:24 +0100

    Update step_1.sh

--- payload/step_1.sh
+++ payload/step_1.sh
@@ -32,12 +32,9 @@ mkdir /root/EXTRAT/firefox-60.0.1esr/
 mkdir $home_py/EXTRAT/firefox-97.0.1/
 
 
-############## firefox-60.0
-wget -q https://ftp.mozilla.org/pub/firefox/releases/60.0.1esr/linux-x86_64/en-US/firefox-60.0.1esr.tar.bz2 && tar -xf firefox-60.0.1esr.tar.bz2 -C $home_py/EXTRAT/firefox-60.0.1esr/
-rm firefox-60.0.1esr.tar.bz2 
-wget -q https://ftp.mozilla.org/pub/firefox/releases/58.0.1/linux-x86_64/en-GB/firefox-58.0.1.tar.bz2 && tar -xf firefox-58.0.1.tar.bz2 -C $home_py/EXTRAT/firefox-58.0.1/
-rm firefox-58.0.1.tar.bz2
+############## firefox-97.0
+
 
  wget -q https://ftp.mozilla.org/pub/firefox/releases/97.0.1/linux-x86_64/en-GB/firefox-97.0.1.tar.bz2 && tar -xf firefox-97.0.1.tar.bz2 -C $home_py/EXTRAT/firefox-97.0.1/
-
+rm firefox-97.0.1.tar.bz2
 echo " * 3 ------------- > INSTALL geckodriver-v0.32.0 "
